e411ce7b57885e337d181da868e051cb23907992
LautiFarias <lfarias@itba.edu.ar>
2023-11-16 19:52:12 -0300

    RestAct

--- TP3HCI/app/src/main/java/com/example/tp3hci/ui/screens/Rest.kt
+++ TP3HCI/app/src/main/java/com/example/tp3hci/ui/screens/Rest.kt
@@ -7,37 +7,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.icons.rounded.PlayArrow
-import androidx.compose.material.icons.rounded.ShoppingCart
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.tp3hci.R
 import com.example.tp3hci.ui.theme.FOrange
 
@@ -45,13 +39,15 @@ import com.example.tp3hci.ui.theme.FOrange
 @Preview(showSystemUi = true)
 @Composable
 fun Rest() {
+    var isTimerPaused by remember { mutableStateOf(false) }
+
+
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
     ) {
-
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -63,11 +59,9 @@ fun Rest() {
                 painter = painterResource(id = R.drawable.logo),
                 contentDescription = null,
                 modifier = Modifier.size(70.dp)
-
             )
-
-
         }
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -79,11 +73,9 @@ fun Rest() {
                 painter = painterResource(id = R.drawable.sentado),
                 contentDescription = null,
                 modifier = Modifier.size(300.dp)
-
             )
-
-
         }
+
 
 
         Row(
@@ -94,26 +86,31 @@ fun Rest() {
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             IconButton(onClick = { /* Manejar acción de repetición */ }) {
-                Icon(Icons.Default.Refresh, contentDescription = "Repetición",tint = FOrange )
+                Icon(Icons.Default.Refresh, contentDescription = "Repetición", tint = FOrange)
             }
             IconButton(onClick = { /* Manejar acción de flecha hacia atrás */ }) {
-                Icon(Icons.Default.ArrowBack, contentDescription = "Flecha hacia atrás",tint = FOrange )
+                Icon(Icons.Default.ArrowBack, contentDescription = "Flecha hacia atrás", tint = FOrange)
             }
 
-            IconButton(onClick = { /* Manejar acción de pausa */ }) {
-                Icon(Icons.Default.PlayArrow, contentDescription = "Pausar",tint = FOrange )
+            IconButton(onClick = {
+                // Manejar acción de pausa
+                isTimerPaused = !isTimerPaused
+            }) {
+                val icon = if (isTimerPaused) {
+                    Icons.Default.Close
+                } else {
+                    Icons.Default.PlayArrow
+                }
+                Icon(icon, contentDescription = "Pausar", tint = FOrange)
             }
 
             IconButton(onClick = { /* Manejar acción de siguiente */ }) {
-                Icon(Icons.Default.ArrowForward, contentDescription = "Flecha hacia adelante",tint = FOrange )
+                Icon(Icons.Default.ArrowForward, contentDescription = "Flecha hacia adelante", tint = FOrange)
             }
 
             IconButton(onClick = { /* Manejar acción de siguiente */ }) {
-                Icon(Icons.Default.List, contentDescription = "Listar" ,tint = FOrange  )
+                Icon(Icons.Default.List, contentDescription = "Listar", tint = FOrange)
             }
         }
-        }
-
     }
-
-
+}
--- TP3HCI/app/src/main/java/com/example/tp3hci/ui/screens/SearchScreen.kt
+++ TP3HCI/app/src/main/java/com/example/tp3hci/ui/screens/SearchScreen.kt
@@ -3,9 +3,18 @@ package com.example.tp3hci.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,15 +23,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tp3hci.R
 import com.example.tp3hci.ui.components.CardItem
 import com.example.tp3hci.ui.components.SearchBar
 import com.example.tp3hci.ui.theme.Black
 import com.example.tp3hci.ui.theme.FOrange
-@Preview
+@Preview(showSystemUi = true)
+
 @Composable
 fun SearchScreen() {
     var searchQuery by remember { mutableStateOf("") }
@@ -40,10 +53,29 @@ fun SearchScreen() {
             fontSize = 30.sp,
             color = FOrange
         )
-        SearchBar(onSearchTextChanged = { newQuery ->
-            searchQuery = newQuery
-            // Puedes realizar acciones de búsqueda aquí según la nueva consulta
-        })
+
+        Row(
+            modifier = Modifier,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Icon(
+                Icons.Default.List,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(34.dp)
+                    .padding(start = 10.dp),
+                tint = Color.White // Ajusta el color del icono según tus necesidades
+            )
+            // Barra de búsqueda
+            SearchBar(onSearchTextChanged = { newQuery ->
+                searchQuery = newQuery
+                // Realizar acciones de búsqueda según la nueva consulta
+            })
+            // Agrega el icono a la izquierda de la barra de búsqueda
+
+
+        }
 
         // Lista de tarjetas
         CardItem(imageResId = R.drawable.gym1, title = "Tarjeta 1", description = "Descripción de la tarjeta 1")
@@ -51,6 +83,5 @@ fun SearchScreen() {
         CardItem(imageResId = R.drawable.gym3, title = "Tarjeta 3", description = "Descripción de la tarjeta 3")
         CardItem(imageResId = R.drawable.gym4, title = "Tarjeta 4", description = "Descripción de la tarjeta 4")
         CardItem(imageResId = R.drawable.gym5, title = "Tarjeta 5", description = "Descripción de la tarjeta 5")
-        // Añade más tarjetas según sea necesario
     }
 }
